cf187f165c8c61b5d1a5391db0c808a5aa912242
Dylan Wicker <dylanwicker@Dylans-MacBook-Air.local>
2023-12-08 15:27:16 -0500

    got puppy names on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
-function App() {
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-     
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
